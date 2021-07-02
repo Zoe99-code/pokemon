b5ac4340dd8caa52438c8bbd651bf38d3bd0e21f
Zoe <lizzystrachan99@gmail.com>
2021-07-02 14:42:25 +0200

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function getPokemonList(url) {
       pokemon.forEach((btn) => {
         container.innerHTML += `<button onclick="getPokemonInfo('${btn.url}')">${btn.name}</button>`;
       });
-      container.innerHTML += `<br><br><button onclick="getPokemonList('${data.next}')">Next</button>`;
+      container.innerHTML += `<button onclick="getPokemonList('${data.next}')">Next</button>`;
+      container.innerHTML += `<button onclick="getPokemonList('${data.previous}')">Previous</button>`;
     });
 }
 getPokemonList(base_URL);
@@ -19,8 +20,19 @@ function getPokemonInfo(url) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-    document.querySelector(".pokemon-info").innerHTML = `
-    <img src="${data.sprites.front_default} ">
+      document.querySelector(".pokemon-info").innerHTML = `
+    <img src="${data.sprites.other.dream_world.front_default} ">
     `;
+      document.querySelector(".pokemon-info").innerHTML += `<br><span> ${
+        "Weight:" + data.weight + "kg" + ""
+      }</span>`;
+
+      document.querySelector(".pokemon-info").innerHTML += `<br><span> ${
+        "Height:" + data.height + "m" + ""
+      }</span>`;
+
+      document.querySelector(".pokemon-info").innerHTML += `<br><span> ${
+        "Name:" + data.name + "" + ""
+      }</span>`;
     });
 }
